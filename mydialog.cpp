49ce8eda96b8369dd20bf3c8c071bedd51b4f661
Adidas05050 <47518015+Adidas05050@users.noreply.github.com>
2019-04-26 15:52:41 +0700

    Add new forms

--- mydialog.cpp
+++ mydialog.cpp
@@ -9,12 +9,13 @@ void MyDialog::on_pushButton_2_clicked()
 {
     QFile file("text.txt");
     file.open(QIODevice::WriteOnly | QIODevice::Text );
+    QString temp = lineEdit_2->text();
+    QString to_file = plainTextEdit->toPlainText();
+    QStringList strList = to_file.split(QRegExp("[\n]"), QString::SkipEmptyParts);
 
-    QString temp = plainTextEdit->toPlainText();
-    QStringList strList = temp.split(QRegExp("[\n]"), QString::SkipEmptyParts);
-
+    lineEdit->setText(temp);
     QTextStream writeStream(&file);
 
-    writeStream << temp << endl;
+    writeStream << to_file << endl;
     file.close();
 }
