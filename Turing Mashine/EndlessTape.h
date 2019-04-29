e7e15381adc12bed44b4066c28602e918887b80b
Tony <siya1337@ro.ru>
2019-04-29 17:40:19 +0600

    Endless tape interface
    First implementation of endless tape data type interface

---
+++ Turing Mashine/EndlessTape.h
@@ -0,0 +1,45 @@
+#ifndef ENDLESSTAPE_H_INCLUDED
+#define ENDLESSTAPE_H_INCLUDED
+
+class EndlessTape
+{
+private:
+
+    enum{SYMBOLS_IN_CHUNK = 32};
+
+    struct DataChunk
+    {
+        char Symbols[SYMBOLS_IN_CHUNK];
+
+        DataChunk(): Symbols{}, PrevChunk(nullptr), NextChunk(nullptr){}
+
+        DataChunk * PrevChunk;
+        DataChunk * NextChunk;
+    };
+
+    uint8_t PositionInChunk;
+    DataChunk * GlobalPosition;
+
+    uint64_t ChunksNumber;
+    DataChunk * FirstChunk;
+    DataChunk * LastChunk;
+
+    char * StringBuffer;
+
+public:
+
+    EndlessTape(EndlessTape &) = delete;
+    EndlessTape(EndlessTape &&) = delete;
+    EndlessTape & operator=(EndlessTape &) = delete;
+    EndlessTape & operator=(EndlessTape &&) = delete;
+
+    EndlessTape();
+    void operator=(const char * String);
+    ~EndlessTape();
+
+    void MoveLeft();
+    void MoveRight();
+    void PutSymbol(char Symbol);
+};
+
+#endif // ENDLESSTAPE_H_INCLUDED
